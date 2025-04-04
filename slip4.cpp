188e2308e84f71d9071872c75beadc2d7abbea69
Jayesh Pandhare <144468239+jayesh410@users.noreply.github.com>
2025-04-04 08:54:51 +0000

    slip 4

--- slip4.cpp
+++ slip4.cpp
@@ -4,4 +4,29 @@ using namespace std;
 inline double areaCircle(double radius){
     return 3.14*radius*radius;
 }
-in
+inline double areaSquare(double side){
+    return side*side;
+
+}
+
+inline double areaReactangle(double lenght,double width){
+    return lenght*width;
+
+}
+
+int main (){
+    double radius,side,lenght,width;
+    cout<<"enter the radius of circle";
+    cin>>radius;
+    cout<<"area of circle"<<areaCircle(radius)<<endl;
+
+    cout<<"enter the side of the square";
+    cin>>side;
+    cout<<"square"<<areaSquare(side)<<endl;
+
+    cout<<"enter lenght and width";
+    cin>>lenght>>width;
+    cout<<"area of reactangle"<<areaReactangle(lenght,width)<<endl;
+
+    return 0;
+}
